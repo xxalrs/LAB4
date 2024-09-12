7b97216d995416bac01a74a16e294eb3f674ac16
Evgeniy <148543433+Evgeniy0y0@users.noreply.github.com>
2024-09-12 15:55:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+## --------------------- Сєваті Євген КН-31\1
+text = ("Whether you're travelling to the islands or the mountains of Thailand, " ## Створення тексту
+         "you're likely to spend at least one night in its capital city on the way."
+        " Bangkok might be noisy and polluted but it's also an exciting city with plenty of things to see and do. "
+         "Why not make it a longer stay?")
+print(text, "\n") ## Вивід тексту у консоль
+print(text.lower(), "\n") ## Вивід тексту нижнім регістром букв
+print("Amount of letter 'a' in text = ", text.count('a') + text.count('A'), "\n") ## Вивід кількість букв "а" у тексті
+print("Is our text lowercased? ", text.islower(), "\n") ## Вивід чи написаний вхідний текст нижнім регістром букв
+## ---------------------
